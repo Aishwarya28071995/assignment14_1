60696292b13502c78bea3c554e6111b0225d5300
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-24 13:42:33 +0530

    Commiting UC1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+
+            stack.Display();
+        }
+    }
+}
